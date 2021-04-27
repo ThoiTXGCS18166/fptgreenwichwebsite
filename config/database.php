2dbc35d1a62d5f82c5173a58b91df1bb5f7ddb77
Tran Xuan Thoi <ThoiTXGCS18166@fpt.edu.vn>
2021-04-27 19:47:00 +0700

    The new change

--- config/database.php
+++ config/database.php
@@ -5,9 +5,12 @@
   $password="78c739da";
   $db="heroku_0a876b33f00670d";
   
-  $conn = mysqli_connect($host,$user,$password,$db);
-
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+  
+// show error
+catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
 ?>
