7651faa1542b05d50f62e76f34f3289a4fa0378b
Tran Xuan Thoi <ThoiTXGCS18166@fpt.edu.vn>
2021-04-27 19:31:00 +0700

    The last change

--- config/database.php
+++ config/database.php
@@ -5,12 +5,9 @@
   $password="78c739da";
   $db="heroku_0a876b33f00670d";
   
-try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-}
-  
-// show error
-catch(PDOException $exception){
-    echo "Connection error: " . $exception->getMessage();
-}
+  $conn = mysqli_connect($host,$user,$password,$db);
+
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
 ?>
